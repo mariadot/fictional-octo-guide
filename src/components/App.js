65d357e4865fb839c90e9305e5c6c21cd92d5a37
mariadot <mariadot.github@gmail.com>
2018-10-14 23:58:14 -0500

    feat: Added Leadboard component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Leaderboard from './Leaderboard';
+
+import '../App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Leaderboard />
+      </div>
+    );
+  }
+}
+
+export default App;
