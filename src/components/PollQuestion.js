d3fd503ad45d49d902e43e678a4b72d4d2ef9583
mariadot <mariadot.github@gmail.com>
2018-10-15 21:04:12 -0500

    feat: Added PollQuestion component and UI

---
+++ src/components/PollQuestion.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class PollQuestion extends Component {
+    render(){
+        return (
+            <div className='new-question ui three column stackable center aligned grid'>
+                <div className='column'>
+                    <div class="ui cards">
+                        <div class="card">
+                            <div class="content">
+                                <div class="header">
+                                    <img class='ui avatar tiny circular image' src='https://lorempixel.com/output/cats-q-c-200-200-9.jpg' />
+                                    <span>Username asks</span>
+                                </div>
+                            </div>
+                            <div class="content">
+                                    <h2>Would you rather...?</h2>
+                                    <div className='field'>
+                                        <div class="ui radio checkbox">
+                                            <input name="radio" checked="checked" type="radio" />
+                                            <label>Option 1</label>
+                                        </div>
+                                    </div>
+                                    <div className='field'>
+                                        <div class="ui radio checkbox">
+                                            <input name="radio" type="radio" />
+                                            <label>Option 2</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            <div class="extra content">
+                                <button class="ui basic green button">
+                                    Submit your answer
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PollQuestion;
