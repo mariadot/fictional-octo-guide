15a188e4475b1fb3d0a21c50f64d948bac4134d2
mariadot <mariadot.github@gmail.com>
2018-10-19 23:08:27 -0500

    feat: Add react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from  'react-redux';
 import { handleGetUsers as getUsers } from '../actions/users';
 import Nav from './Nav';
@@ -7,6 +8,7 @@ import NewQuestion from './NewQuestion';
 import QuestionPoll from './QuestionPoll';
 import QuestionResults from './QuestionResults';
 import Question from './Question';
+import Login from './Login';
 import LoadingBar from 'react-redux-loading';
 
 import '../App.css';
@@ -18,14 +20,29 @@ class App extends Component {
     }
 
     render() {
+        const { authUser } = this.props;
+        console.log(this.props);
             return (
-                <div className='ui container'>
-                    <LoadingBar />
-                    <Nav/>
-                    <Leaderboard />
-                </div>
+                <Router>
+                    <Fragment>
+                        <div className='ui container'>
+                            <LoadingBar />
+                            <Nav />
+                            <Route path='/' exact component={ authUser.length > 0 ? Question : Login } />
+                            <Route path='/leaderboard' component={Leaderboard} />
+                            <Route path='/new-question' component={NewQuestion} />
+                    </div>
+                    </Fragment>
+                </Router>
         );
     }
 }
 
-export default connect()(App);
+function mapStateToProps({authUser, users}){
+    return {
+        authUser,
+        users
+    }
+}
+
+export default connect(mapStateToProps)(App);
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Nav extends Component {
     render(){
         return (
-            <div className="ui secondary pointing menu">
-                <a className="active item">
-                    Home
-                </a>
-                <a className="item">
-                    New Question
-                </a>
-                <a className="item">
-                    Leader Board
-                </a>
-                <div className="right menu">
-                    <a className="ui item">
-                        Logout
-                    </a>
-                </div>
-            </div>
+            <nav className='nav'>
+                <ul className='ui secondary pointing menu'>
+                    <li>
+                        <NavLink to='/' exact className='item' activeClassName='active'>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/leaderboard' className='item' activeClassName='active'>
+                            Leaderboard
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/new-question' className='item' activeClassName='active'>
+                            New Question
+                        </NavLink>
+                    </li>
+                    <li className='right menu'>
+                        <NavLink to='/logout' className='ui item' activeClassName='active'>
+                            Logout
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
         )
     }
 }
