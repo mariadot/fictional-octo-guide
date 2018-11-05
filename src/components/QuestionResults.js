d02a2ee6ea164b81e903c28c2fc541d67bac1d4f
mariadot <mariadot.github@gmail.com>
2018-11-04 23:55:49 -0500

    feat: Implement Question Results component and view

--- src/components/QuestionResults.js
+++ src/components/QuestionResults.js
@@ -1,47 +1,58 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class QuestionResults extends Component {
     render(){
+        const { userAnswer, question, author } = this.props;
+        const optionOne = question.optionOne ? question.optionOne.text : '';
+        const optionTwo = question.optionTwo ? question.optionTwo.text : '';
+        const allVotes = question.optionOne.votes.length + question.optionTwo.votes.length;
+        const optionOneVotes = ( question.optionOne.votes.length / allVotes) * 100;
+        const optionTwoVotes = (question.optionTwo.votes.length / allVotes) * 100;
+        
+
         return (
             <div className='new-question ui three column stackable center aligned grid'>
                 <div className='column'>
-                    <div class="ui cards">
-                        <div class="card">
-                            <div class="content">
-                                <div class="header">
-                                    <img class='ui avatar tiny circular image' alt='user avatar' src='https://lorempixel.com/output/cats-q-c-200-200-9.jpg' />
-                                    <span>Asked by User</span>
+                    <div className="ui cards">
+                        <div className="card">
+                            <div className="content">
+                                <div className="header">
+                                    <img className='ui avatar tiny circular image' alt='user avatar' src='https://lorempixel.com/output/cats-q-c-200-200-9.jpg' />
+                                    <span>Asked by {author}</span>
                                 </div>
                             </div>
-                            <div class="content">
-                            <h2>Results:</h2>
-                                <div className='ui basic segment'>
-                                    <div>Would you rather fly?</div>
-                                    <div class="ui small progress">
-                                        <div class="bar">
-                                            <div class="progress"></div>
+                            <div className="content">
+                            <h2>Results: {userAnswer}</h2>
+                                <div className='ui segment'>
+                                    { userAnswer == 'optionOne' &&
+                                        <div className="ui olive right corner label">
+                                            <i className='check icon'></i>
+                                        </div>
+                                    }
+                                    <div>{optionOne}?</div>
+                                    <div className="ui small progress">
+                                        <div className="bar"  style={{width: optionOneVotes+'%'}}>
+                                            <div className="progress">{optionOneVotes}%</div>
                                         </div>
                                         
                                     </div>
-                                    <span>1 out of 2 votes</span>
+                                    <span>{question.optionOne.votes.length} out of {allVotes} votes</span>
                                 </div>
-                                <div className='ui olive segment'>
-                                <a href='#' class="ui olive right corner label">
-                                    <i className='check icon'></i>
-                                </a>
-                                    <div class="label">Would you rather be invisible?</div>
-                                    <div class="ui small progress" >
-                                        <div class="bar" style={{width: '50%'}}>
-                                            <div class="progress">50%</div>
+                                <div className='ui segment'>
+                                    { userAnswer == 'optionTwo' &&
+                                        <div className="ui olive right corner label">
+                                            <i className='check icon'></i>
+                                        </div>
+                                    }
+                                    <div className="label">{optionTwo}?</div>
+                                    <div className="ui small progress" >
+                                        <div className="bar" style={{width: optionTwoVotes+'%'}}>
+                                            <div className="progress">{optionTwoVotes}%</div>
                                         </div>
                                     </div>
-                                    <span>1 out of 2 votes</span>
+                                    <span>{question.optionTwo.votes.length} out of {allVotes} votes</span>
                                 </div>
-                            </div>
-                            <div class="extra content">
-                                <button class="ui basic green button">
-                                    Submit your answer
-                                </button>
                             </div>
                         </div>
                     </div>
@@ -51,4 +62,19 @@ class QuestionResults extends Component {
     }
 }
 
-export default QuestionResults;
+function mapStateToProps({users, questions}, props){
+    const { id } = props;
+    const currentQuestion = questions[id];
+    const questionAuthor = currentQuestion ? currentQuestion.author : '';
+    const author = users[questionAuthor] ? users[questionAuthor].name : '';
+    const userAnswer = users[questionAuthor].answers[id];
+
+    return {
+        author: author,
+        question: currentQuestion ? currentQuestion : {},
+        userAnswer: userAnswer,
+    }
+}
+
+
+export default connect(mapStateToProps)(QuestionResults);
