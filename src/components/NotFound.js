e0c8c6c6cb9148b2c0bf8ae1e41fa71502ab9e65
mariadot <mariadot.github@gmail.com>
2018-11-12 12:19:12 -0500

    fix: Suggestion; add link to go back home from 404

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div>
             <h1>The content you're looking for does not exist</h1>
+            <p>You can go back Home <Link to='/' exact>here</Link></p>
         </div>
     )
 }
